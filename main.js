353e79d8273b0bae260e6c66c75c5338d0468334
twiickcom <twiick.com@gmail.com>
2023-07-02 17:37:50 -0500

    Se agrega la sección del resumen del carrito, al menú. Se evita que pueda quedar más de un menú abierto de forma simultanea.

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const menuEmail=document.querySelector(".navbar-email");
 const desktopMenu=document.querySelector(".desktop-menu");
 const menuHamIcon=document.querySelector(".menu")
 const mobileMenu=document.querySelector(".mobile-menu")
-menuEmail.addEventListener("click", toogleDesktopMenu);
-menuHamIcon.addEventListener("click", toogleMobileMenu)
 
-function toogleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive");
+const menuCarritoIcon=document.querySelector(".navbar-shopping-cart");
+const aside=document.querySelector(".product-detail");
+
+menuEmail.addEventListener("click", function(){toogle(desktopMenu)});
+menuHamIcon.addEventListener("click", function(){toogle(mobileMenu)});
+menuCarritoIcon.addEventListener("click", function(){toogle(aside)});
+
+function toogle(element){
+    desactivar(desktopMenu, element);
+    desactivar(mobileMenu, element);
+    desactivar(aside, element);
+   element.classList.toggle("inactive");
 }
 
-function toogleMobileMenu(){
-    mobileMenu.classList.toggle("inactive");
+function desactivar(element, element2){
+    if(!element.classList.contains("inactive") && element!=element2){
+        element.classList.add("inactive");
+    }
 }
